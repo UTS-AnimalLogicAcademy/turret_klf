2c6d65a5944c36726b405aa5c7be9b7fe3768470
jjonahlowercase <jonah@jonahnewton.com.au>
2022-05-09 14:16:20 +1000

    Added variant for katana-4.5.2

--- package.py
+++ package.py
@@ -2,9 +2,9 @@
 
 name = 'turret_klf'
 
-version = '1.1.12'
+version = '1.1.13'
 
-authors = ['daniel.flood', 'ben.skinner', 'wen.tan', 'naomi.que']
+authors = ['daniel.flood', 'ben.skinner', 'wen.tan', 'naomi.que', 'jonah.newton']
 
 build_requires = [
                   'python',
@@ -13,8 +13,9 @@ build_requires = [
 ]
 
 variants = [
-   ['platform-linux', 'arch-x86_64', 'katana-3.0.7', 'tbb-4'],
+    ['platform-linux', 'arch-x86_64', 'katana-3.0.7', 'tbb-4'],
     ['platform-linux', 'arch-x86_64', 'katana-3.6.2', 'tbb_katana-2017'],
+    ['platform-linux', 'arch-x86_64', 'katana-4.5.2', 'tbb_katana-2019.6']
 ]
 
 def commands():
